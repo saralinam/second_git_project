31cbe81fe89c3b00014d78ea92e154d64a4ff913
saralina11! <smouwakeh1@gmail.com>
2023-03-12 13:14:00 -0500

    add change

--- src/main/java/Git_Second_Project/SecondClass.java
+++ src/main/java/Git_Second_Project/SecondClass.java
@@ -2,6 +2,6 @@ package Git_Second_Project;
 
 public class SecondClass {
     public static void main(String[] args) {
-        System.out.println("written in sara branc");
+        System.out.println("written in sara branch");
     }
 }
